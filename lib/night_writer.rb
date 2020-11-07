ea07df3993d8c46a1767b9dfffc7197024264100
Sam Yeo <yeosamuel95@gmail.com>
2020-11-06 20:03:02 -0700

    Complete iteration 1 with dynamic CLI char count

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -2,4 +2,7 @@ print "Please enter the name you would like to be translated into braille "
 input_file = gets.chomp
 print "Please enter the name you would like for the braille text file. "
 output_file = gets.chomp
-puts "Created '#{output_file}' containing 256 characters\n"
+read_input_file = File.open(input_file, 'r')
+char_count = read_input_file.read.delete("\n").length
+read_input_file.close
+puts "Created '#{output_file}' containing #{char_count} characters\n"
