af2f1153e186377bcf413ba168337847de5e81c3
Sam Yeo <yeosamuel95@gmail.com>
2020-11-06 19:28:02 -0700

    Add first query for input file

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,3 +1,5 @@
+print "Please enter the name you would like to be translated into braille "
+input_file = gets.chomp
 print "Please enter the name you would like for the braille text file. "
 output_file = gets.chomp
-puts "\nCreated '#{output_file}' containing 256 characters\n"
+puts "Created '#{output_file}' containing 256 characters\n"
