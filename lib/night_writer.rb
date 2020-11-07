0abb8e8456048ff521f74788dca7ecaf5830b38a
Sam Yeo <yeosamuel95@gmail.com>
2020-11-06 19:25:56 -0700

    Add custom name for output txt file

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,2 +1,3 @@
-gets.chomp
-print "Created 'braille.txt' containing 256 characters"
+print "Please enter the name you would like for the braille text file. "
+output_file = gets.chomp
+puts "\nCreated '#{output_file}' containing 256 characters\n"
