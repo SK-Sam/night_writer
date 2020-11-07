eb9888b9209b7c8ff045df43f668ed2ebbf2b505
Sam Yeo <yeosamuel95@gmail.com>
2020-11-07 00:00:40 -0700

    Add three dictionaries to EnToBrailleDict class

--- test/en_to_braille_dict_test.rb
+++ test/en_to_braille_dict_test.rb
@@ -4,11 +4,15 @@ require './lib/en_to_braille_dict'
 class EnToBrailleDictTest < MiniTest::Test
 
     def setup
-        @dictionary = EnToBrailleDict.new
+        @en_to_braille = EnToBrailleDict.new
+    end
+
+    def test_it_can_instantiate_with_a_set_dictionary
+        assert_equal "a", @en_to_braille.dictionary.keys[0]
     end
 
     def test_it_can_translate_single_chars
         string_to_translate = "h"
-        assert_equal "0.\n00\n..", @dictionary.translate(string_to_translate)
+        assert_equal "0.\n00\n..", @en_to_braille.translate(string_to_translate)
     end
 end
