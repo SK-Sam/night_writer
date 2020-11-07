c53eba011efa21dcd9a97d561296d489104f2f79
Sam Yeo <yeosamuel95@gmail.com>
2020-11-06 22:00:43 -0700

    Move input file to root dir, add extra ':' in night_writer

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,6 +1,6 @@
-print "Please enter the name you would like to be translated into braille "
+print "Please enter the name you would like to be translated into braille: "
 input_file = gets.chomp
-print "Please enter the name you would like for the braille text file. "
+print "Please enter the name you would like for the braille text file: "
 output_file = gets.chomp
 read_input_file = File.open(input_file, 'r')
 text_as_string = read_input_file.read
