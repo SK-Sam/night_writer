72bf037ef442441c739d4c8bfde06f99a8959d4b
Sam Yeo <yeosamuel95@gmail.com>
2020-11-07 00:11:02 -0700

    Complete method which writes single char into a file as braille

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,3 +1,5 @@
+require './lib/en_to_braille_dict'
+
 print "Please enter the name you would like to be translated into braille: "
 input_file = gets.chomp
 print "Please enter the name you would like for the braille text file: "
@@ -6,7 +8,8 @@ read_input_file = File.open(input_file, 'r')
 text_as_string = read_input_file.read
 read_input_file.close
 char_count = text_as_string.count "^\n"
+en_to_braille_dict = EnToBrailleDict.new
 File.open(output_file, 'w') do |file|
-    file.write(text_as_string)
+    file.write(en_to_braille_dict.translate(text_as_string[1]))
 end
 puts "Created '#{output_file}' containing #{char_count} characters\n"
