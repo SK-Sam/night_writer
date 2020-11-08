e0daf96bf4f442c7682dc358b74f4dd8373fa05b
Sam Yeo <yeosamuel95@gmail.com>
2020-11-07 19:19:26 -0700

    Add tests for helper methods to translate

--- test/en_to_braille_dict_test.rb
+++ test/en_to_braille_dict_test.rb
@@ -16,15 +16,25 @@ class EnToBrailleDictTest < MiniTest::Test
         assert_equal "0.\n00\n..\n", @en_to_braille.translate(string_to_translate)
     end
 
+    def test_it_can_split_given_string_into_arr_of_braille
+        expected = [["0.", "00", ".."],["0.", "..", ".."]]
+        assert_equal expected, @en_to_braille.arr_of_braille("ha")
+    end
+
+    def test_it_can_separate_braille_array_into_three_lines_format
+        expected = [["0.", "0."], ["00", ".."], ["..", ".."]]
+        assert_equal expected, @en_to_braille.format_into_three_arrays("ha")
+    end
+
+    def test_it_can_turn_three_arrays_into_three_braille_strings
+      expected = "0.0.\n00..\n....\n"  
+      assert_equal expected, @en_to_braille.format_into_three_braille_strings("ha")
+    end
+
     def test_it_can_translate_multiple_chars
         string_to_translate = "ha"
 
         expected = "0.0.\n00..\n....\n"
         assert_equal expected, @en_to_braille.translate(string_to_translate)
-    end
-
-    def test_it_can_split_given_string_into_arr_of_braille
-        expected = [["0.", "00", ".."],["0.", "..", ".."]]
-        assert_equal expected, @en_to_braille.arr_of_braille("ha")
     end
 end
