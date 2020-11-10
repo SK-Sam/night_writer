8518f11df893e631841a6d1dfe6796059e3a843f
Sam Yeo <yeosamuel95@gmail.com>
2020-11-10 05:21:59 -0700

    Correct typo

--- test/reader_test.rb
+++ test/reader_test.rb
@@ -4,7 +4,7 @@ require './lib/reader'
 class ReaderTest < MiniTest::Test
 
     def test_it_can_instantiate_and_ask_user_for_input
-        reader = Reader.take_user_input
+        reader = Reader.take_user_input_en_to_braille
 
         assert_equal "message.txt", reader.input_file
         assert_equal "braille.txt", reader.output_file
