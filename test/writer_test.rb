ec33a148e58f0c934626de05568bc61c9da22b37
Sam Yeo <yeosamuel95@gmail.com>
2020-11-10 06:50:22 -0700

    Add test for writing translation from en to braille

--- test/writer_test.rb
+++ test/writer_test.rb
@@ -10,4 +10,16 @@ class WriterTest < MiniTest::Test
 
         assert_instance_of Writer, writer
     end
+
+    def test_it_can_translate_from_en_to_braille
+        writer = Writer.new("./test/test_writer.txt", "hello world")
+        writer.translate_en_to_braille
+
+        file_content = File.open("./test/test_writer.txt", 'r') do |file|
+            file.read
+        end
+
+        expected = "0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0...\n"
+        assert_equal expected, file_content
+    end
 end
