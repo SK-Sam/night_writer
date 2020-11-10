ffbe72d579f695f2266b364ea37d1b18c491f5b8
SK-Sam <yeosamuel95@gmail.com>
2020-11-09 17:33:50 -0800

    Reformat test cases to test new format of helper methods

--- test/braille_to_en_dict_test.rb
+++ test/braille_to_en_dict_test.rb
@@ -17,16 +17,35 @@ class BrailleToEnDictTest < MiniTest::Test
         expected_string_2 = "....0.......0.......0.......0.......0.......0.......0.......0.......0.......0..."
         expected_string_3 = "................................................................................"
         expected = [expected_string_1, expected_string_2, expected_string_3]
+
         assert_equal expected, @braille_to_en.separate_to_three_lines(string_input)
     end
 
-    def test_it_can_format_three_strings_into_braille_char_format
+    def test_it_can_separate_strings_per_two_chars
+        top_row_string = "0..."
+        middle_row_string = "...."
+        bottom_row_string = "...."
+
+        expected1 = ["0.", ".."]
+        expected2 = ["..", ".."]
+        expected3 = ["..", ".."]
+        assert_equal expected1, @braille_to_en.separate_each_string_per_two_chars(top_row_string)
+        assert_equal expected2, @braille_to_en.separate_each_string_per_two_chars(middle_row_string)
+        assert_equal expected3, @braille_to_en.separate_each_string_per_two_chars(bottom_row_string)
+    end
+
+    def test_it_can_separate_array_of_strings_per_two_chars
         top_row_string = "0..."
         middle_row_string = "...."
         bottom_row_string = "...."
         array_of_three_strings = [top_row_string, middle_row_string, bottom_row_string]
 
-        expected = ["0.\n..\n..", "..\n..\n.."]
-        assert_equal expected, @braille_to_en.format_into_braille_char_array(array_of_three_strings)
+        expected = [
+            ["0.", ".."], 
+            ["..", ".."],
+            ["..", ".."]
+        ]
+        #expected = ["0.\n..\n..", "..\n..\n.."]
+        assert_equal expected, @braille_to_en.separate_array_of_braille_strings_per_two_chars(array_of_three_strings)
     end
 end
