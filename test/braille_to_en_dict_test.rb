e76fa0592b7720a4ebc6fb0f7ed644e44ae0e004
SK-Sam <yeosamuel95@gmail.com>
2020-11-10 01:54:28 -0800

    Add test for helper method to split 80+ char braille file into multiple arrays

--- test/braille_to_en_dict_test.rb
+++ test/braille_to_en_dict_test.rb
@@ -25,6 +25,7 @@ class BrailleToEnDictTest < MiniTest::Test
         top_row_string = "0..."
         middle_row_string = "...."
         bottom_row_string = "...."
+        array_of_rows = [top_row_string, middle_row_string, bottom_row_string]
 
         expected1 = ["0.", ".."]
         expected2 = ["..", ".."]
@@ -97,9 +98,9 @@ class BrailleToEnDictTest < MiniTest::Test
             ["0.", "..", ".."]
         ]  
 
-        expected_row1 = "0.\n..\n..\n"
-        expected_row2 = "0.\n..\n..\n"
-        expected_row3 = "0.\n..\n..\n"
+        expected_row1 = "0.\n..\n.."
+        expected_row2 = "0.\n..\n.."
+        expected_row3 = "0.\n..\n.."
         expected = [expected_row1, expected_row2, expected_row3]
         assert_equal expected, @braille_to_en.braille_as_strings(arr_of_rows)
     end
@@ -108,5 +109,20 @@ class BrailleToEnDictTest < MiniTest::Test
         string_to_translate = "0.0...0.\n........\n........"
 
         assert_equal "aa a", @braille_to_en.translate(string_to_translate)
+    end
+
+    def test_it_can_split_file_of_over_80_chars_into_multiple_arrays_of_80_or_less
+        
+        string_of_305 = "0.0.0.0.0.0.0.0.0...0.0.0.0.0.0.0.0.0...0.0.0.0.0.0.0.0.0...0.0.0.0.0.0.0.0.0...\n................................................................................\n................................................................................\n0.0.0.0.0.0.0.0.0...\n....................\n...................."
+
+        expected = ["0.0.0.0.0.0.0.0.0...0.0.0.0.0.0.0.0.0...0.0.0.0.0.0.0.0.0...0.0.0.0.0.0.0.0.0...\n................................................................................\n................................................................................\n", "0.0.0.0.0.0.0.0.0...\n....................\n...................."]
+        assert_equal expected, @braille_to_en.separate_braille_over_80_chars_into_arrays(string_of_305)
+    end
+
+    def test_it_can_read_braille_file_of_over_80_chars
+        string_of_50_en_chars = "0.0.0.0.0.0.0.0.0...0.0.0.0.0.0.0.0.0...0.0.0.0.0.0.0.0.0...0.0.0.0.0.0.0.0.0...\n................................................................................\n................................................................................\n0.0.0.0.0.0.0.0.0...\n....................\n...................."
+        
+        expected = "aaaaaaaaa aaaaaaaaa aaaaaaaaa aaaaaaaaa aaaaaaaaa "
+        assert_equal expected, @braille_to_en.translate(string_of_50_en_chars)
     end
 end
