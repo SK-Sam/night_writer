60dc45351969cb8502e3f768361c660d3f3a8080
Sam Yeo <yeosamuel95@gmail.com>
2020-11-10 05:56:24 -0700

    Add braille_to_en class method of initializing

--- lib/reader.rb
+++ lib/reader.rb
@@ -9,6 +9,14 @@ class Reader
         new(input_file, output_file)
     end
 
+    def self.take_user_input_braille_to_en
+        print "Please enter the name you would like to be translated into English: "
+        input_file = gets.chomp
+        print "Please enter the name you would like for the English text file: "
+        output_file = gets.chomp
+        new(input_file, output_file)
+    end
+
     def initialize(input_file_name, output_file_name)
         @input_file_name = input_file_name
         @output_file_name = output_file_name
@@ -21,7 +29,8 @@ class Reader
         end
     end
 
-    def file_char_count
+    def file_char_count_en_to_braille
         @input_as_string.length
     end
+
 end
