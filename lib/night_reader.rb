a0dd840c3d2b82174ee904af73220eaf7054aa1a
Sam Yeo <yeosamuel95@gmail.com>
2020-11-10 07:09:21 -0700

    Integrate write class into night_reader

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,10 +1,8 @@
-require './lib/braille_to_en_dict'
 require './lib/reader'
+require './lib/writer'
 
 reader = Reader.take_user_input_braille_to_en
 reader.read_file
-braille_to_en_dict = BrailleToEnDict.new
-File.open(reader.output_file_name, 'w') do |file|
-    file.write(braille_to_en_dict.translate(reader.input_as_string))
-end
+writer = Writer.new(reader.output_file_name, reader.input_as_string)
+writer.translate_braille_to_en
 puts "Created '#{reader.output_file_name}' containing #{reader.file_char_count_braille_to_en} characters\n"
