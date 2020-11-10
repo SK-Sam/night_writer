ee3bdd7117dcb10c4a6f39e27babfbf534f0eae0
Sam Yeo <yeosamuel95@gmail.com>
2020-11-10 05:30:57 -0700

    Add stubs to focus on asking for user input

--- test/reader_test.rb
+++ test/reader_test.rb
@@ -5,8 +5,10 @@ class ReaderTest < MiniTest::Test
 
     def test_it_can_instantiate_and_ask_user_for_input
         reader = Reader.take_user_input_en_to_braille
+        reader.stubs(:input_file_name).returns("message.txt")
+        reader.stubs(:output_file_name).returns("braille.txt")
 
-        assert_equal "message.txt", reader.input_file
-        assert_equal "braille.txt", reader.output_file
+        assert_equal "message.txt", reader.input_file_name
+        assert_equal "braille.txt", reader.output_file_name
     end
 end
