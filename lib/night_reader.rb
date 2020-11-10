7e53afc803bb45008b7eb6b59d24f6e9b542eea2
Sam Yeo <yeosamuel95@gmail.com>
2020-11-10 07:37:48 -0700

    Implement summarize method in night_reader and night_writer

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -5,4 +5,4 @@ reader = Reader.take_user_input_braille_to_en
 reader.read_file
 writer = Writer.new(reader.output_file_name, reader.input_as_string)
 writer.translate_braille_to_en
-puts "Created '#{reader.output_file_name}' containing #{reader.file_char_count_braille_to_en} characters\n"
+puts reader.summarize("night_reader")
--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -5,4 +5,4 @@ reader = Reader.take_user_input_en_to_braille
 reader.read_file
 writer = Writer.new(reader.output_file_name, reader.input_as_string)
 writer.translate_en_to_braille
-puts "Created '#{reader.output_file_name}' containing #{reader.file_char_count_en_to_braille} characters\n"
+puts reader.summarize("night_writer")
