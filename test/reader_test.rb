68a00385fd243c388d6d180868a19902aa15f3a1
Sam Yeo <yeosamuel95@gmail.com>
2020-11-10 06:04:20 -0700

    Add instantiation method for night_reader

--- test/reader_test.rb
+++ test/reader_test.rb
@@ -25,6 +25,15 @@ class ReaderTest < MiniTest::Test
         assert_equal 11, reader.file_char_count_en_to_braille
     end
 
+    def test_it_can_instantiate_differently_in_night_reader
+        reader = Reader.take_user_input_braille_to_en
+        reader.stubs(:input_file_name).returns("braille.txt")
+        reader.stubs(:output_file_name).returns("message.txt")
+
+        assert_equal "braille.txt", reader.input_file_name
+        assert_equal "message.txt", reader.output_file_name
+    end
+
     def test_it_can_find_file_char_count_for_braille_to_en
         reader = Reader.new("braille.txt", "./test/test.txt")
 
