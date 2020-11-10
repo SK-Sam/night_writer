b4bed7839b1f4bcb152aca6a3abc8c0e8425466c
Sam Yeo <yeosamuel95@gmail.com>
2020-11-10 05:20:26 -0700

    Add instantiation test using class method which asks for input

---
+++ test/reader_test.rb
@@ -0,0 +1,12 @@
+require './test/test_helper'
+require './lib/reader'
+
+class ReaderTest < MiniTest::Test
+
+    def test_it_can_instantiate_and_ask_user_for_input
+        reader = Reader.take_user_input
+
+        assert_equal "message.txt", reader.input_file
+        assert_equal "braille.txt", reader.output_file
+    end
+end
