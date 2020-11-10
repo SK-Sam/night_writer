15f6ec5ed6694cb5c9ee4bfc3fe0b30aefaef08e
Sam Yeo <yeosamuel95@gmail.com>
2020-11-10 05:55:24 -0700

    Change name of method due to shared namespace

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -7,4 +7,4 @@ en_to_braille_dict = EnToBrailleDict.new
 File.open(reader.output_file_name, 'w') do |file|
     file.write(en_to_braille_dict.translate(reader.input_as_string))
 end
-puts "Created '#{reader.output_file_name}' containing #{reader.file_char_count} characters\n"
+puts "Created '#{reader.output_file_name}' containing #{reader.file_char_count_en_to_braille} characters\n"
